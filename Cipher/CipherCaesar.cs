d837695168e6fb088b25089f959f4bafbf89c048
BagStrider <andrey64931@gmail.com>
2022-01-02 14:32:06 +0300

    delete delegate from cipher ceaser and create cipher trisemus

--- Cipher/CipherCaesar.cs
+++ Cipher/CipherCaesar.cs
@@ -11,19 +11,14 @@ namespace Cipher
     {
         private List<char> alphabet = new List<char>() { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
         private List<char> inscryptAlphabet = new List<char>() { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
-        delegate void CipherCaesarDelegate();
         private Regex isLetter = new Regex(@"[a - zA - Z]");
-        private CipherCaesarDelegate ccd;
         private StringBuilder _text;
-
         public int Offset { get; set; }
-
-
+    
         public CipherCaesar(string text, int offset)
         {
             _text = new StringBuilder(text.ToLower());
             Offset = offset;
-            ccd = InscryptByOffset;
         }
 
         public CipherCaesar(string text, int offset, string keyWord)
@@ -34,18 +29,13 @@ namespace Cipher
 
         public void Inscrypt()
         {
-            ccd.Invoke();
-        }
-        private void InscryptByOffset()
-        {
             InscryptAlphabetByOffset();
-            for(int i =0; i < _text.Length; i++)
+            for (int i = 0; i < _text.Length; i++)
             {
                 if (isLetter.IsMatch(_text[i].ToString()))
                     continue;
                 _text[i] = inscryptAlphabet[alphabet.IndexOf(_text[i])];
             }
-            
         }
 
         private void InscryptAlphabetByOffset()
@@ -60,6 +50,5 @@ namespace Cipher
         {
             return _text.ToString();
         }
-
     }
 }
---
+++ Cipher/CipherTrisemus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cipher
+{
+    internal class CipherTrisemus
+    {
+        private List<char> alphabet = new List<char>() { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+        private List<char> inscryptAlphabet = new List<char>();
+        private Regex isLetter = new Regex(@"[a - zA - Z]");
+        private StringBuilder _text;
+        private string _keyWord;
+
+
+        public CipherTrisemus(string text, string keyWord)
+        {
+            _text = new StringBuilder(text.ToLower());
+            _keyWord = keyWord;
+        }
+
+
+
+
+
+    }
+}
